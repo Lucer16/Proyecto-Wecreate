2156a66d7c4e0f9085af6d72b22cdbe0a5739384
Lucero Agrazal <yeivent@gmail.com>
2025-03-18 16:32:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.querySelector(".transition-container").classList.add("show");
 });
+
+// Carrusel de Iconos Inicio
+document.addEventListener("DOMContentLoaded", function () {
+    const carruselContainer = document.querySelector(".habilidades-a");
+
+    // Duplicamos las imágenes para lograr el efecto infinito
+    const duplicado = carruselContainer.innerHTML;
+    carruselContainer.innerHTML += duplicado;
+});
+
+// Carrusel de Iconos Sobre Mi
+document.addEventListener("DOMContentLoaded", function () {
+    const carruselContainer = document.querySelector(".carrusel-container");
+
+    // Duplicamos las imágenes para lograr el efecto infinito
+    const duplicado = carruselContainer.innerHTML;
+    carruselContainer.innerHTML += duplicado;
+});
+
